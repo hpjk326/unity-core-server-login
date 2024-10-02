66c6ef0261160a0cf89e3d104c1530fbae891414
hpjk326 <hpjk326@naver.com>
2024-10-03 00:27:32 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using DotnetCoreServer.Models;
 
 namespace DotnetCoreServer
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IWebHostEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -22,34 +21,52 @@ namespace DotnetCoreServer
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc()
-               .AddJsonOptions(options =>
-               {
-                   options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
-               });;
-            
-            services.AddSingleton<IConfiguration>(Configuration);
-            services.AddSingleton<IUserDao,UserDao>();
-            services.AddSingleton<IStageResultDao,StageResultDao>();
-            services.AddSingleton<IRankDao,RankDao>();
-            services.AddSingleton<IUpgradeDao,UpgradeDao>();
-            services.AddSingleton<IDB,DB>();
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+                });
 
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddSingleton<IUserDao, UserDao>();
+            services.AddSingleton<IStageResultDao, StageResultDao>();
+            services.AddSingleton<IRankDao, RankDao>();
+            services.AddSingleton<IUpgradeDao, UpgradeDao>();
+            services.AddSingleton<IDB, DB>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-            app.UseMvc();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
